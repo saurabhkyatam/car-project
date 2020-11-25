d6109abfdaf9afec5ffd7703714f71522d59644c
Saurabh Kyatam <saurabhkyatam77@gmail.com>
2020-11-25 18:04:42 +0530

    add constructer

--- src/manage/dealer.java
+++ src/manage/dealer.java
@@ -1,5 +1,12 @@
 package manage;
 
 public class dealer {
+	int dealerid;
+	String name;
+	public dealer(int dealerid, String name) {
+		super();
+		this.dealerid = dealerid;
+		this.name = name;
+	}
 
 }
